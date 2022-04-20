75a436d02ac6f03a2874363dcceed9d9093be810
AlexLadino <jair.ladino@correounivalle.edu.co>
2022-04-20 01:29:03 -0500

    1st commit

---
+++ allrunPimple.bash
@@ -0,0 +1,26 @@
+#!/bin/sh
+cd ${0%/*} || exit 1
+
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+# indique el solver pimpleXFoam
+rm -r 2.00000000
+rm -r 4.00000000
+rm -r 6.00000000
+rm -r 8.00000000
+rm -r postProcessing
+rm -r processor*
+rm -r log.*
+rm -r constant/polyMesh/sets
+
+numProc=6
+cp  ./system/decomposeParDict.orig	./system/decomposeParDict
+UFile="./system/decomposeParDict"
+sed s/"\(numberOfSubdomains[ \t]*\) 4"/"\1 $numProc"/g $UFile > temp.$$
+mv -f temp.$$ $UFile
+
+topoSet
+decomposePar
+mpiexec -np $numProc renumberMesh -overwrite -parallel
+
+mpiexec -np $numProc $1 -parallel
